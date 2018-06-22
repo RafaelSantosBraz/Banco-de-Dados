57d1597d0c27fca51a543e1910429647d373478a
Rafael dos Santos Braz <rafinha.santos.braz@gmail.com>
2018-06-22 11:37:19 -0300

    continuação de insert -- sem correção

--- Lista 1/CC2018_DML.sql
+++ Lista 1/CC2018_DML.sql
@@ -14,6 +14,50 @@ INSERT INTO Autores
 VALUES
     (4, 'Enriquerson Rodrigues', 'abbcd');
 
+INSERT INTO Autores
+VALUES
+    (5, 'Pablito Hermanito', 'aacd');
+
 INSERT INTO Editoras
 VALUES
-    (1, '', '', '');
+    (1, 'Companhia Das Letras', 'Rua Estados Unidos', '86360000');
+
+INSERT INTO Editoras
+VALUES
+    (2, 'Ediouro', 'Rua José Caciola', '17123000');
+
+INSERT INTO Editoras
+VALUES
+    (3, 'Editora 34', 'Rua Hungria, 592, Jardim Europa', '01455000');
+
+INSERT INTO Editoras
+VALUES
+    (4, 'Rocco', 'Av. Pres. Wilson, 231 – 8º andar', '20030021');
+
+INSERT INTO Editoras
+VALUES
+    (5, 'Intrínseca', 'Rua Marquês de São Vicente, 99 / 3º andar Gávea - Rio de Janeiro - RJ', '22451041');
+
+INSERT INTO PalavrasChave
+VALUES
+    ('ficção');
+
+INSERT INTO PalavrasChave
+VALUES
+    ('romance');
+
+INSERT INTO PalavrasChave
+VALUES
+    ('fantástica');
+
+INSERT INTO PalavrasChave
+VALUES
+    ('clássica');
+
+INSERT INTO PalavrasChave
+VALUES
+    ('juvenil');
+
+INSERT INTO Livros
+VALUES
+    (1, '85-359-0644-4', 'O Cosmo de Einstein', 2005, 'primeira', 1, 1);
