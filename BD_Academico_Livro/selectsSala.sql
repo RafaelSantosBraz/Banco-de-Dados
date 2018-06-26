6952b1c813fc3a100419489a4c01a780cb717ccb
RafaelSantosBraz <rafinha.santos.braz@gmail.com>
2018-06-26 17:31:38 -0300

    finalização dos selects

--- BD_Academico_Livro/selectsSala.sql
+++ BD_Academico_Livro/selectsSala.sql
@@ -41,3 +41,25 @@ select current_date - dt_nasc "Quantidade de Dias" from aluno;
 
 select * from aluno;
 
+select nome, sexo, cidade, bairro from aluno where cidade = 'Londrina' and (bairro = 'Centro' or bairro =  'Barra Suja');
+
+select nome, sexo, cidade, bairro from aluno where cidade = 'Londrina' and (bairro = 'Centro' or bairro =  'Barra Suja') order by cidade, nome;
+
+select nome, sexo, cidade, bairro from aluno where cidade = 'Londrina' and bairro IN ('Centro', 'Barra Suja') order by cidade, nome;
+
+select nome, sexo, cidade, bairro from aluno where cidade = 'Londrina' and bairro IN (select bairro from aluno where matric = 'A31') order by cidade, nome;
+
+select matric, cod_disc, nota_final from aluno_na_disc where nota_final between 6.1 and 8.5 order by nota_final;
+
+select matric, cod_disc, nota_final from aluno_na_disc where nota_final not between 6.1 and 8.5 order by nota_final;
+
+select nome, cidade from aluno where nome like 'A%' order by 1;
+
+select nome, cidade from aluno where nome ilike 'a%' order by 1;
+
+select nome, cidade from aluno where nome ilike 'a%a' order by 1;
+
+select nome, cidade from aluno where nome ilike '%na%' order by 1;
+
+select nome, cidade from aluno where nome like '%A__ton%' order by 1;
+
